167c21522e3a34891d2fdf716ee663188bfcf7a3
TakahashiYoshida <takahashiyoshida28@gmail.com>
2020-11-27 23:33:46 +0900

    first

--- run_training.py
+++ run_training.py
@@ -51,7 +51,7 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
     train.mirror_augment = mirror_augment
     train.image_snapshot_ticks = train.network_snapshot_ticks = 10
     sched.G_lrate_base = sched.D_lrate_base = 0.002
-    sched.minibatch_size_base = 32
+    sched.minibatch_size_base = 4
     sched.minibatch_gpu_base = 4
     D_loss.gamma = 10
     metrics = [metric_defaults[x] for x in metrics]
